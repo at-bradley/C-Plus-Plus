ef8a9de446be3a0d43b1e5370a98cf26908cb803
Anthony Thudium <thudius.anthony@gmail.com>
2017-09-17 22:06:59 -0700

    Update PrataListings_3.cpp

--- PrataListings_3.cpp
+++ PrataListings_3.cpp
@@ -320,3 +320,23 @@ int main() {
     return 0;
 }
 */
+
+//modulus.cpp
+
+/*
+int main() {
+    
+    const int Lbs_per_stn = 14;
+    int lbs;
+    
+    cout << "Enter your weight in pounds: ";
+    cin >> lbs;
+    
+    int stone = lbs / Lbs_per_stn;
+    int pounds = lbs % Lbs_per_stn;
+    
+    cout << lbs << " pounds are " << stone << " stone, " << pounds << "pound(s).\n";
+    
+    return 0;
+}
+*/
